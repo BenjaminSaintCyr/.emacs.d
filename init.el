72b4d7990ff65cf312ba023d8ba7272f33ca7098
Benjamin Saint-Cyr <benjamin.saintcyr@gmail.com>
2020-10-25 21:16:27 -0400

    usable workflow

--- init.el
+++ init.el
@@ -51,11 +51,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-blank-before-new-entry '((heading) (plain-list-item)))
  '(package-selected-packages
-   '(avy flycheck-grammarly flycheck company ivy-rich ivy winum use-package evil doom-modeline dashboard auto-package-update)))
+   '(evil-magit gcmh dap-mode lsp-ui lsp-mode elfeed-org elfeed counsel magit avy flycheck-grammarly flycheck company ivy-rich ivy winum use-package evil doom-modeline dashboard auto-package-update)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(lsp-ui-doc-background ((t (:background nil))))
+ '(lsp-ui-doc-header ((t (:inherit (font-lock-string-face italic))))))
