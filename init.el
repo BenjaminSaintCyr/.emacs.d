6b24556a76350a77df6d7328146747ffc520f3bb
Benjamin Saint-Cyr <benjamin.saintcyr@gmail.com>
2020-10-28 12:00:07 -0400

    switch pm to straight

--- init.el
+++ init.el
@@ -4,48 +4,33 @@
       package-archives
       '(("gnu"   . "https://elpa.gnu.org/packages/")
 	("melpa" . "https://melpa.org/packages/")))
-;; -MelpaPackages
 
-;; ConfigurePackageManager
-(unless (bound-and-true-p package--initialized)
-  (setq package-enable-at-startup nil)          ; To prevent initializing twice
-  (package-initialize))
+;;; use straigth package manager
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+;;;; Integration with use-package
+(straight-use-package 'use-package)
 
-;; set use-package-verbose to t for interpreted .emacs,
-;; and to nil for byte-compiled .emacs.elc.
-(eval-and-compile
-  (setq use-package-verbose (not (bound-and-true-p byte-compile-current-file))))
-;; -ConfigurePackageManager
-
-;; ConfigureUsePackage
-;; Install use-package if not installed
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-(eval-and-compile
-  (setq use-package-always-ensure t)
-  (setq use-package-expand-minimally t)
-  (setq use-package-compute-statistics t)
-  (setq use-package-enable-imenu-support t))
-
-(eval-when-compile
-  (require 'use-package)
-  (require 'bind-key))
-;; -ConfigureUsePackage
-
-;; AutoPackageUpdate
-(use-package auto-package-update
-	     :if (not (daemonp))
-	     :custom
-	     (auto-package-update-interval 7) ;; in days
-	     (auto-package-update-prompt-before-update t)
-	     (auto-package-update-delete-old-versions t)
-	     (auto-package-update-hide-results t)
-	     :config
-	     (auto-package-update-maybe))
+;;; Better garbage colletion
+(use-package gcmh
+:straight t
+:ensure t
+  :config
+  (gcmh-mode 1))
 
 (org-babel-load-file "~/.emacs.d/config.org")
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -53,7 +38,7 @@
  ;; If there is more than one, they won't work right.
  '(org-blank-before-new-entry '((heading) (plain-list-item)))
  '(package-selected-packages
-   '(evil-magit gcmh dap-mode lsp-ui lsp-mode elfeed-org elfeed counsel magit avy flycheck-grammarly flycheck company ivy-rich ivy winum use-package evil doom-modeline dashboard auto-package-update)))
+   '(ac-octave evil-magit gcmh dap-mode lsp-ui lsp-mode elfeed-org elfeed counsel magit avy flycheck-grammarly flycheck company ivy-rich ivy winum use-package evil doom-modeline dashboard auto-package-update)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
